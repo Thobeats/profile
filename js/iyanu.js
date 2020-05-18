ea6439f389a67929f7c044e1a197162c1b772dd7
Thobeats <41216440+Thobeats@users.noreply.github.com>
2020-05-18 12:10:03 +0100

    new commit

---
+++ js/iyanu.js
@@ -0,0 +1,86 @@
+$(document).ready(function(){
+
+    // var slides = document.getElementsByClassName("slide");
+    // var links = document.getElementsByClassName("link");
+    // var slideIndex = 1;
+    // showSlides(slideIndex);
+    
+    // function currentSlide(n) {
+    //     showSlides(slideIndex = n);
+    // }
+    
+    // function showSlides(n) {
+    //     var i;
+       
+    //     if (n > slides.length) {slideIndex = 1}    
+    //     if (n < 1) {slideIndex = slides.length}
+    //     for (i = 0; i < slides.length; i++) {
+    //         slides[i].style.display = "none";  
+    //     }
+    //     for (i = 0; i < links.length; i++) {
+    //         links[i].className = links[i].className.replace(" active", "");
+    //     }
+    //     slides[slideIndex-1].style.display = "block";  
+    //     links[slideIndex-1].className += " active";
+    // }
+
+
+    // $(".nav-list").each(function(){ 
+    //     var link = $(this).attr("data-target");
+    //     link.hide();
+
+    //    $(this).click(function(){
+          
+    //        $("section").each(function(){
+    //            var id = $(this).attr("id");
+    //            var Id = "#"+id;
+
+    //            if (link == Id){
+    //             $(link).show().effect("slide", 500);
+    //            } 
+    //        });           
+    //    });
+
+    // });
+
+
+    $("#content-2").hide();
+    $("#content-3").hide();
+    $("#content-4").hide();
+
+   $("#home").click(function(){
+        $("#content-4").hide();
+        $("#content-2").hide();
+        $("#content-3").hide();
+        $("#content-1").effect("slide", 500);
+   })
+
+   
+   $("#gallery").click(function(){
+       $("#content-4").hide(); 
+       $("#content-1").hide();
+       $("#content-3").hide();
+       $("#content-2").effect("slide", 500);
+   });
+
+   $("#project").click(function(){
+       $("#content-1").hide();
+       $("#content-2").hide();
+       $("#content-4").hide();
+       $("#content-3").effect("slide", 500);
+   });
+
+   $("#contact-me").click(function(){
+        $("#content-1").hide();
+        $("#content-2").hide();
+        $("#content-3").hide();
+        $("#content-4").effect("slide", 500);
+   });
+   
+
+   
+});
+
+
+
+
